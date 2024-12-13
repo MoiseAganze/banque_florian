be220235687d669eb76ec99d0735b728adec6569
Dev-Florian-Diangongo <floriandiangongo22@gmail.com>
2024-12-13 16:26:31 +0100

    j ai commence par la gestion de users

--- backend/banque/User/views.py
+++ backend/banque/User/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework import generics
+from .models import User
+from .serializers import UserRegistrationSerializer
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
