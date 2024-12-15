5b023cf28feb8240939efb9ff0508e615fa2bff8
Dev-Florian-Diangongo <floriandiangongo22@gmail.com>
2024-12-15 09:54:22 +0100

    j ai recommence le projet backend comme on s est attendu hier

--- backend/banque/User/views.py
+++
@@ -1,9 +0,0 @@
-
-
-from rest_framework import generics
-from .models import User
-from .serializers import UserRegistrationSerializer
-
-class UserRegistrationView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserRegistrationSerializer
