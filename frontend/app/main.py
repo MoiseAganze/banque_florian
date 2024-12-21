37be3e7a4ead38775c1622f518f45869e5cba4cd
MoiseAganze <aganzedev@gmail.com>
2024-12-21 20:14:46 +0100

    nouveau front

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -5,7 +5,7 @@ from Custom_Widgets import *
 from Custom_Widgets.QAppSettings import QAppSettings
 from src.Function import GuiFunction
 from Custom_Widgets.QCustomQToolTip import QCustomQToolTipFilter
-from PySide6.QtCore import QSettings,QTimer,Qt,QPoint
+from PySide6.QtCore import QSettings, QTimer, Qt, QPoint
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -16,66 +16,44 @@ class MainWindow(QMainWindow):
         ########################################################################
         # APPLY JSON STYLESHEET
         ########################################################################
-        # self = QMainWindow class
-        # self.ui = Ui_MainWindow / user interface class
-        #Use this if you only have one json file named "style.json" inside the root directory, "json" directory or "jsonstyles" folder.
-        # loadJsonStyle(self, self.ui) 
-
-        # Use this to specify your json file(s) path/name
-        loadJsonStyle(self, self.ui, jsonFiles = {
-            "json-styles/style.json"
-            }) 
+        loadJsonStyle(self, self.ui, jsonFiles={"json-styles/style.json"})
 
         ########################################################################
-
-        #######################################################################
         # SHOW WINDOW
-        #######################################################################
-        self.show() 
-
         ########################################################################
-        # UPDATE APP SETTINGS LOADED FROM JSON STYLESHEET 
-        # ITS IMPORTANT TO RUN THIS AFTER SHOWING THE WINDOW
-        # THIS PROCESS WILL RUN ON A SEPARATE THREAD WHEN GENERATING NEW ICONS
-        # TO PREVENT THE WINDOW FROM BEING UNRESPONSIVE
-        ########################################################################
-        # self = QMainWindow class
-        QAppSettings.updateAppSettings(self)
-        self.app_functions=GuiFunction(self)
-        self.old_pos=None
-       
-
-
-    def sassCompilationProgress(self, n):
-        self.ui.activityProgress.setValue(n)
-    # def mousePressEvent(self,event):
         
-    #     if event.button()==Qt.LeftButton and self.childAt(event.pos())==self.ui.header: 
-    #         self.old_pos=event.globalPos()
+        self.show()
+        
+        ########################################################################
+        # UPDATE APP SETTINGS LOADED FROM JSON STYLESHEET
+        ########################################################################
+        QAppSettings.updateAppSettings(self)
+        self.app_functions = GuiFunction(self)
+        
+    # def mousePressEvent(self, event):
+    #     if event.button() == Qt.LeftButton and self.childAt(event.pos()) == self.ui.header:
+    #         # Calculer le décalage entre la position de la souris et le coin supérieur gauche de la fenêtre
+    #         self.old_pos = event.globalPos() - self.frameGeometry().topLeft()
+    #         self._is_moving = True
     #         print("header clicked")
-    # def mouseMoveEvent(self,event):
-    #     if self.old_pos:
-    #         delta=QPoint(event.globalPos()-self.old_pos)
-    #         self.move(self.x()+delta.x(),self.y()+delta.y())
-    #         self.old_pos=event.globalPos()
+
+    # def mouseMoveEvent(self, event):
+    #     if self._is_moving and self.old_pos is not None:
+    #         # Déplacer la fenêtre en maintenant le décalage initial
+    #         self.move(event.globalPos() - self.old_pos)
     #         print("header moved")
-    # def mouseReleaseEvent(self,event):
-    #     if event.button()==Qt.LeftButton:
-    #         self.old_pos=None
+
+    # def mouseReleaseEvent(self, event):
+    #     if event.button() == Qt.LeftButton:
+    #         self._is_moving = False
+    #         self.old_pos = None
     #         print("header released")
-########################################################################
-## EXECUTE APP
-########################################################################
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ########################################################################
-    ## 
-    ########################################################################
-    app_tooltip_filter=QCustomQToolTipFilter(tailPosition="auto")
+    app_tooltip_filter = QCustomQToolTipFilter(tailPosition="auto")
     app.installEventFilter(app_tooltip_filter)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
-########################################################################
-## END===>
-########################################################################  
+    
+    sys.exit(app.exec())
