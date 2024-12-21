0a25030abe3bc6777258bab1638fdce29a46cb8d
Dev-Florian-Diangongo <floriandiangongo22@gmail.com>
2024-12-21 10:41:39 +0100

    j'ai construit un client pyqt5 qui se loge et logout

--- backend/banque/User/urls.py
+++ backend/banque/User/urls.py
@@ -4,6 +4,6 @@ from .views import UserTokenObtainPairView,UserTokenRefresh,CreateUserView, logi
 urlpatterns = [
     path("api/token/", UserTokenObtainPairView.as_view(), name="userapitoken"),
     path("api/token/refresh/", UserTokenRefresh.as_view(), name="api_refresh_token"),
-    path("CreateUserview/", CreateUserView.as_view(), name="CreateUserview"),
+    path("inscription_user/", CreateUserView.as_view(), name="CreateUserview"),
     path("login_user/", login_user, name="login_user"),
 ]
